ede102c103ad5b766f37e37877b0916e380b6afe
alam2015 <alamnaryab@gmail.com>
2017-05-25 02:52:02 +0500

    bootstrap ccs,js and fontawsome and simple index page blank

---
+++ index.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="en-us">
+<head>
+    <title>Project</title>
+    <meta http-equiv="Content-Type" content="text/html;charset=UTF-8">
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    
+    <meta name="description" content="Project_description">
+    <meta name="keywords" content="keywords">
+    <meta name="author" content="Fakhr-e-Alam">
+    
+    <link rel="icon" href="img/favicon.png" type="image/png" />
+    
+    <link rel="stylesheet" type="text/css" href="css/bootstrap.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/font-awesome.min.css" />
+    <link rel="stylesheet" type="text/css" href="css/style.css" />
+    
+    <script type="text/javascript" src="js/jquery.min.js"></script>
+    <script type="text/javascript" src="js/bootstrap.min.js"></script>
+    <script type="text/javascript" src="js/script.js"></script>
+    
+    <!-- HTML5 shiv and Respond.js IE8 support for HTML5 elements and media queries -->
+    <!--[if IE]>
+    <script src="https://oss.maxcdn.com/html5shiv/3.7.2/html5shiv.min.js"></script>
+    <script src="https://oss.maxcdn.com/respond/1.4.2/respond.min.js"></script>
+    <![endif]-->
+</head>
+<body>
+    <div class="alert alert-success">
+        Working both bootstrap
+        <br>fontawsome <i class="fa fa-home"></i>
+        <br>glyphicon <i class="glyphicon glyphicon-user"></i>
+    </div>
+</body>
+</html>
